cf5f24c8438de86264afef828f1f8a713a703d7a
Manjari Dhoundiyal <manjari.dhoundiyal@hotmail.com>
2024-02-01 17:56:28 +0000

    Events added

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -4,28 +4,41 @@ import '../calculator.css'
 const Calculator = () =>{
 
     const [result, setResult] = useState<number>(0);
+    const [initialVal, setInitialVal] = useState<number>(0);
+    const [expression, setExpression] = useState<string | null>(null);
+    
     console.log(setResult);
+
+    const addNumber = (evt: React.MouseEvent<HTMLElement>) =>{
+        let calculatedScreenVal = initialVal + (evt?.target as HTMLElement)?.innerHTML;
+        setInitialVal(+calculatedScreenVal)
+    }
+
+    const addExpression = (evt: React.MouseEvent<HTMLElement>) => {
+        
+    }
+
     return (
         <div className="calculator-layout">
-            <div className='calculator-result'>{result}</div>
+            <div className='calculator-result'>{initialVal}</div>
             <div className="calculator-keys">
                 <div className="key first">AC</div>
                 <div className="key first">+/-</div>
                 <div className="key first">%</div>
                 <div className="key active">÷</div>
-                <div className="key">7</div>
-                <div className="key">8</div>
-                <div className="key">9</div>
+                <div onClick={addNumber} className="key">7</div>
+                <div onClick={addNumber} className="key">8</div>
+                <div onClick={addNumber} className="key">9</div>
                 <div className="key active">&times;</div>
-                <div className="key">4</div>
-                <div className="key">5</div>
-                <div className="key">6</div>
+                <div onClick={addNumber} className="key">4</div>
+                <div onClick={addNumber} className="key">5</div>
+                <div onClick={addNumber} className="key">6</div>
                 <div className="key active">-</div>
-                <div className="key">1</div>
-                <div className="key">2</div>
-                <div className="key">3</div>
+                <div onClick={addNumber} className="key">1</div>
+                <div onClick={addNumber} className="key">2</div>
+                <div onClick={addNumber} className="key">3</div>
                 <div className="key active">+</div>
-                <div className='two-span key'>0</div>
+                <div onClick={addNumber} className='two-span key'>0</div>
                 <div className="key">.</div>
                 <div className="key active">=</div>
             </div>
